da19f7f447888fd611ae1f1cc52467dd014a407b
JM Bae <jungman82@gmail.com>
2023-09-11 13:40:36 +0900

    User 모델 생성

--- core/user/models.py
+++ core/user/models.py
@@ -1,3 +1,31 @@
+import uuid
+
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
 
-# Create your models here.
+
+class UserManager:
+    pass
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    public_id = models.UUIDField(db_index=True, unique=True, default=uuid.uuid4(), editable=False)
+    username = models.CharField(db_index=True, max_length=255, unique=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(db_index=True, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+    objects = UserManager()
+
+    def __str__(self):
+        return f'{self.email}'
+
+    @property
+    def name(self):
+        return f"{self.first_name} {self.last_name}"
+    
