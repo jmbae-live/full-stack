f2473e717cfd52c14a363b87499473fd4ad4099a
JM Bae <jungman82@gmail.com>
2023-09-13 10:31:21 +0900

    Post serializers.py viewsets.py 추가

--- core/routers.py
+++ core/routers.py
@@ -1,6 +1,7 @@
 from rest_framework import routers
 
 from core.auth.viewsets import RegisterViewSet, LoginViewSet, RefreshViewSet
+from core.post.viewsets import PostViewSet
 from core.user.viewsets import UserViewSet
 
 router = routers.SimpleRouter()
@@ -8,6 +9,9 @@ router.register(r'user', UserViewSet, basename='user')
 router.register(r'auth/register', RegisterViewSet, basename='auth-register')
 router.register(r'auth/login', LoginViewSet, basename='auth-login')
 router.register(r'auth/refresh', RefreshViewSet, basename='auth-refresh')
+
+# POST
+router.register(r'post', PostViewSet, basename='post')
 
 urlpatterns = [
     *router.urls,
