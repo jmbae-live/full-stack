ea7b1b762177363030204062670fcad50d584a7e
JM Bae <jungman82@gmail.com>
2023-09-14 11:25:09 +0900

    토큰 만료시간 연장 8시간으로 (기본값 5분)

--- CoreRoot/settings.py
+++ CoreRoot/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -137,5 +137,6 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=8),
     'UPDATE_LAST_LOGIN': True
 }
