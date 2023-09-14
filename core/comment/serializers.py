20bf49f9358314f91cbcf48ab3e844ebf533cef9
JM Bae <jungman82@gmail.com>
2023-09-14 13:32:35 +0900

    댓글 수정

--- core/comment/serializers.py
+++ core/comment/serializers.py
@@ -19,6 +19,19 @@ class CommentSerializer(AbstractSerializer):
             raise ValidationError("You can't create a post for another user.")
         return value
 
+    def validate_post(self, value):
+        if self.instance:
+            return self.instance.post
+        return value
+
+    def update(self, instance, validated_data):
+        if not instance.edited:
+            validated_data['edited'] = True
+
+        instance = super().update(instance, validated_data)
+
+        return instance
+
     def to_representation(self, instance):
         rep = super().to_representation(instance)
         author = User.objects.get_object_by_public_id(rep["author"])
