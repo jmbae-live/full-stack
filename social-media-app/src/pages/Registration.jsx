62614dd090d48b7bd62c05fb54d5b5218cec277a
JM Bae <jungman82@gmail.com>
2023-09-18 14:34:16 +0900

    누락된 페이지 스타일 추가

--- social-media-app/src/pages/Registration.jsx
+++ social-media-app/src/pages/Registration.jsx
@@ -9,14 +9,16 @@ function Registration() {
 		<div className="container">
 			<div className="row">
 				<div className="col-md-6 d-flex align-items-center">
-					<h1 className="text-primary">Welcome to Postman!</h1>
-					<p className="content">
-						This is a new social media site that will allow you to share your
-						thoughts and experiences with your friends. Register now and start
-						enjoying! <br />
-						Or if you already have an account, please
-						<Link to="/login/"></Link>
-					</p>
+					<div className="content text-center px-4">
+						<h1 className="text-primary">Welcome to Postman!</h1>
+						<p className="content">
+							This is a new social media site that will allow you to share your
+							thoughts and experiences with your friends. Register now and start
+							enjoying! <br />
+							Or if you already have an account, please
+							<Link to="/login/"></Link>
+						</p>
+					</div>
 				</div>
 				<div className="col-md-6 p-5">
 					<RegistrationForm />
