0fdfd7dcc22ccd4dcaf9454bac96b16b1fd6ca81
JM Bae <jungman82@gmail.com>
2023-09-18 10:38:40 +0900

    ProtectedRoute 추가

--- social-media-app/src/App.js
+++ social-media-app/src/App.js
@@ -1,12 +1,21 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
+import React from "react"
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import ProtectedRoute from "./routes/ProtectedRoute"
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Home/>} />
-    </Routes>
-  );
+	return (
+		<Routes>
+			<Route
+				path="/"
+				element={
+					<ProtectedRoute>
+						<Home />
+					</ProtectedRoute>
+				}
+			/>
+      <Route path="/login/" element={<div>Login</div>} />
+		</Routes>
+	)
 }
-export default App;
+export default App
