895c7236f1a7fa8f9145b4bbf7757852b8a2d411
JM Bae <jungman82@gmail.com>
2023-09-19 14:40:16 +0900

    Post Detail Component 추가

---
+++ social-media-app/src/components/posts/Post.jsx
@@ -0,0 +1,82 @@
+import React from "react"
+import { Card, Image } from "react-bootstrap"
+import { format } from "timeago"
+import { randomAvatar } from "../../utils"
+
+import { LikeFilled, LikeOutlined } from "@ant-design/icons"
+
+function Post(props) {
+	const { post, refresh } = props
+
+	const handleLikeClick = (action) => {}
+
+	return (
+		<>
+			<Card className="rounded-3 my-4">
+				<Card.Body>
+					<Card.Title className="d-flex flex-row justify-content-between">
+						<div className="d-flex flex-row">
+							<Image
+								src={randomAvatar()}
+								roundedCircle
+								width={48}
+								height={48}
+								className="me-2 border border-primary
+                          border-2"
+							/>
+							<div className="d-flex flex-column justify-content-start align-self-center mt-2">
+								<p className="fs-6 m-0">{post.author.name}</p>
+								<p className="fs-6 fw-lighter">
+									<small>{format(post.created)}</small>
+								</p>
+							</div>
+						</div>
+					</Card.Title>
+					<Card.Text>{post.body}</Card.Text>
+					<div className="d-flex flex-row">
+						<LikeFilled
+							style={{
+								color: "#fff",
+								backgroundColor: "#0D6EFD",
+								borderRadius: "50%",
+								width: "18px",
+								height: "18px",
+								fontSize: "75%",
+								padding: "2px",
+								margin: "3px",
+							}}
+						/>
+						<p className="ms-1 fs-6">
+							<small>{post.likes_count} like</small>
+						</p>
+					</div>
+				</Card.Body>
+				<Card.Footer className="d-flex bg-white w-50 justify-content-between border-0">
+					<div className="d-flex flex-row">
+						<LikeOutlined
+							style={{
+								width: "24px",
+								height: "24px",
+								padding: "2px",
+								fontSize: "20px",
+								color: post.liked ? "#0D6EFD" : "#C4C4C4",
+							}}
+							onClick={() => {
+								if (post.liked) {
+									handleLikeClick("remove_like")
+								} else {
+									handleLikeClick("like")
+								}
+							}}
+						/>
+						<p className="ms-1">
+							<small>Like</small>
+						</p>
+					</div>
+				</Card.Footer>
+			</Card>
+		</>
+	)
+}
+
+export default Post
