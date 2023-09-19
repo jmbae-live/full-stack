7c1cb8df9f993f414662eed35060bcb69c81d16d
JM Bae <jungman82@gmail.com>
2023-09-19 09:14:03 +0900

    custom user action hook 추가

---
+++ social-media-app/src/hooks/user.actions.js
@@ -0,0 +1,35 @@
+import axios from "axios"
+import { useNavigate } from "react-router-dom"
+
+function useUserActions() {
+  const navigate = useNavigate()
+  const baseURL = 'http://localhost:8000/api'
+  return {
+    login,
+    // register,
+    logout,
+  }
+
+  function setUserData(data) {
+    localStorage.setItem('auth', JSON.stringify({
+      access: data.access,
+      refresh: data.refresh,
+      user: data.user,
+    }))
+  }
+
+  function login(data) {
+    return axios.post(`${baseURL}/auth/login/`, data).then((res) => {
+      setUserData(data)
+      navigate('/')
+    })
+  }
+
+  function logout() {
+    localStorage.removeItem("auth");
+    navigate("/login");
+  }
+  
+}
+
+export default useUserActions
