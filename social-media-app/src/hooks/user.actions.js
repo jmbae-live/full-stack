f617dc2fb400cff76ae3cdfc8a272ede8f43d13d
JM Bae <jungman82@gmail.com>
2023-09-19 10:10:29 +0900

    export 구문 오류 수정, 코드 서식 재지정

--- social-media-app/src/hooks/user.actions.js
+++ social-media-app/src/hooks/user.actions.js
@@ -2,63 +2,64 @@ import axios from "axios"
 import { useNavigate } from "react-router-dom"
 
 function useUserActions() {
-  const navigate = useNavigate()
-  const baseURL = 'http://localhost:8000/api'
-  return {
-    login,
-    register,
-    logout,
-  }
+	const navigate = useNavigate()
+	const baseURL = "http://localhost:8000/api"
+	return {
+		login,
+		register,
+		logout,
+	}
 
-  function setUserData(data) {
-    localStorage.setItem('auth', JSON.stringify({
-      access: data.access,
-      refresh: data.refresh,
-      user: data.user,
-    }))
-  }
+	function setUserData(data) {
+		localStorage.setItem(
+			"auth",
+			JSON.stringify({
+				access: data.access,
+				refresh: data.refresh,
+				user: data.user,
+			})
+		)
+	}
 
-  function register(data) {
-    return axios.post(`${baseURL}/auth/register/`, data).then((res) => {
-      setUserData(res.data);
-      navigate("/");
-    });
-  }
+	function register(data) {
+		return axios.post(`${baseURL}/auth/register/`, data).then((res) => {
+			setUserData(res.data)
+			navigate("/")
+		})
+	}
 
-  function login(data) {
-    return axios.post(`${baseURL}/auth/login/`, data).then((res) => {
-      setUserData(res.data)
-      navigate('/')
-    })
-  }
+	function login(data) {
+		return axios.post(`${baseURL}/auth/login/`, data).then((res) => {
+			setUserData(res.data)
+			navigate("/")
+		})
+	}
 
-  function logout() {
-    localStorage.removeItem("auth");
-    navigate("/login");
-  }
-  
+	function logout() {
+		localStorage.removeItem("auth")
+		navigate("/login")
+	}
 }
 
 function getUser() {
-  const auth = JSON.parse(localStorage.getItem("auth")) || null;
-  if (auth) {
-    return auth.user;
-  } else {
-    return null;
-  }
+	const auth = JSON.parse(localStorage.getItem("auth")) || null
+	if (auth) {
+		return auth.user
+	} else {
+		return null
+	}
 }
-
 
 // Get the access token
 function getAccessToken() {
-  const auth = JSON.parse(localStorage.getItem("auth"));
-  return auth.access;
+	const auth = JSON.parse(localStorage.getItem("auth"))
+	return auth.access
 }
 
 // Get the refresh token
 function getRefreshToken() {
-  const auth = JSON.parse(localStorage.getItem("auth"));
-  return auth.refresh;
+	const auth = JSON.parse(localStorage.getItem("auth"))
+	return auth.refresh
 }
 
-export default {useUserActions, getUser, getAccessToken, getRefreshToken}
+export { useUserActions, getUser, getAccessToken, getRefreshToken }
