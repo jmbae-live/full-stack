663ba413c4279504151210e4a48e62762c49ae2f
JM Bae <jungman82@gmail.com>
2023-09-22 14:48:20 +0900

    profile 링크 수정

--- social-media-app/src/components/Navbar.jsx
+++ social-media-app/src/components/Navbar.jsx
@@ -1,35 +1,37 @@
-import React from "react"
-import { Container, Nav, NavDropdown, Navbar, Image } from "react-bootstrap"
-import { useUserActions } from "../hooks/user.actions"
-import { randomAvatar } from "../utils";
+import React from 'react';
+import {Container, Image, Nav, Navbar, NavDropdown} from 'react-bootstrap';
+import {getUser, useUserActions} from '../hooks/user.actions';
+import {randomAvatar} from '../utils';
+import {Link} from 'react-router-dom';
 
 function Navigationbar() {
   const userActions = useUserActions();
-
+  const user = getUser();
   const handleLogout = () => {
-    userActions.logout()
-  }
-	return (
-		<Navbar bg="primary" variant="dark">
-			<Container>
-				<Navbar.Brand className="fw-bold" href="#home">
-					Postagram
-				</Navbar.Brand>
-				<Navbar.Collapse className="justify-content-end">
-					<Nav>
-						<NavDropdown
-							title={<Image src={randomAvatar()} roundedCircle width={36} height={36} />}
-						>
-              <NavDropdown.Item href="#">Profile
-             </NavDropdown.Item>
-             <NavDropdown.Item onClick={handleLogout}>
-               Logout</NavDropdown.Item>
-            </NavDropdown>
-					</Nav>
-				</Navbar.Collapse>
-			</Container>
-		</Navbar>
-	)
+    userActions.logout();
+  };
+  return (
+      <Navbar bg="primary" variant="dark">
+        <Container>
+          <Navbar.Brand className="fw-bold" href="#home">
+            Postagram
+          </Navbar.Brand>
+          <Navbar.Collapse className="justify-content-end">
+            <Nav>
+              <NavDropdown
+                  title={<Image src={randomAvatar()} roundedCircle width={36}
+                                height={36}/>}
+              >
+                <NavDropdown.Item as={Link} to={`/profile/${user.id}/`}>Profile
+                </NavDropdown.Item>
+                <NavDropdown.Item onClick={handleLogout}>
+                  Logout</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+  );
 }
 
-export default Navigationbar
+export default Navigationbar;
