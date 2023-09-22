64e6ecf9a649cb020a485f910ee7d5967b20130e
JM Bae <jungman82@gmail.com>
2023-09-22 13:41:40 +0900

    Profile 페이지 구성

--- social-media-app/src/App.js
+++ social-media-app/src/App.js
@@ -1,33 +1,42 @@
-import React from "react"
-import { Route, Routes } from "react-router-dom"
-import Home from "./pages/Home"
-import Login from "./pages/Login"
-import Registration from "./pages/Registration"
-import ProtectedRoute from "./routes/ProtectedRoute"
-import SinglePost from "./pages/SinglePost"
+import React from 'react';
+import {Route, Routes} from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Registration from './pages/Registration';
+import ProtectedRoute from './routes/ProtectedRoute';
+import SinglePost from './pages/SinglePost';
+import Profile from './pages/Profile';
 
 function App() {
-	return (
-		<Routes>
-			<Route
-				path="/"
-				element={
-					<ProtectedRoute>
-						<Home />
-					</ProtectedRoute>
-				}
-			/>
-			<Route path="/login/" element={<Login />} />
-			<Route path="/register/" element={<Registration />} />
-			<Route
-				path="/post/:postId/"
-				element={
-					<ProtectedRoute>
-						<SinglePost />
-					</ProtectedRoute>
-				}
-			/>
-		</Routes>
-	)
+  return (
+      <Routes>
+        <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <Home/>
+              </ProtectedRoute>
+            }
+        />
+        <Route
+            path="/post/:postId/"
+            element={
+              <ProtectedRoute>
+                <SinglePost/>
+              </ProtectedRoute>
+            }
+        />
+        <Route
+            path="/profile/:profileId/"
+            element={
+              <ProtectedRoute>
+                <Profile/>
+              </ProtectedRoute>
+            }/>
+        <Route path="/login/" element={<Login/>}/>
+        <Route path="/register/" element={<Registration/>}/>
+      </Routes>
+  );
 }
-export default App
+
+export default App;
