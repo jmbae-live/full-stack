da842fe836891c548148731ccfdadcc862684f6d
JM Bae <jungman82@gmail.com>
2023-09-22 11:28:07 +0900

    중괄호 오류 수정

--- social-media-app/src/components/MoreToggleIcon.jsx
+++ social-media-app/src/components/MoreToggleIcon.jsx
@@ -2,13 +2,17 @@ import { MoreOutlined } from "@ant-design/icons";
 import React from "react";
 import { Link } from "react-router-dom";
 
-const MoreToggleIcon = React.forwardRef(({onClick}, ref) => {
-  <Link to='#' ref={ref} onClick={(e) => {
-    e.preventDefault()
-    onClick(e);
-  }}>
+const MoreToggleIcon = React.forwardRef(({ onClick }, ref) => (
+  <Link
+    to="#"
+    ref={ref}
+    onClick={(e) => {
+      e.preventDefault();
+      onClick(e);
+    }}
+  >
     <MoreOutlined />
   </Link>
-})
+));
 
-export default MoreToggleIcon
+export default MoreToggleIcon;
